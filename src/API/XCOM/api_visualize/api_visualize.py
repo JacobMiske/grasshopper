1c87a8c5b122df32f0ec524709d5b5d9da84b31a
Jacob Miske <jacobmiske@gmail.com>
2020-02-12 09:34:50 -0500

    chore: regenerating venv

---
+++ src/API/XCOM/api_visualize/api_visualize.py
@@ -0,0 +1,23 @@
+
+
+class api_visualize(object):
+    """
+    Object for all APIs to use to pass results towards.
+
+    Able to generate plots and mathematical relations.
+    """
+    def __init__(self):
+        self.desc = "API visualization tools"
+
+    def plot_list(self):
+        """
+        Plots list of values given a title and units for x, y, z, etc.
+        :return:
+        """
+        return 0
+
+    def get_metrics_on_list(self):
+        """
+        Takes a data stream and converts to comparative values.
+        :return:
+        """
